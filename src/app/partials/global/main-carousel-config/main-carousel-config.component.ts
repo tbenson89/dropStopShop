772e83e0bd0331c51876e8ff3dcc1744641082ea
A. Tyler Benson <typrobenson@gmail.com>
2020-05-14 13:33:18 -0600

    Add owl-carousel-module works need to make dynamic~~~!

--- src/app/partials/global/main-carousel-config/main-carousel-config.component.ts
+++ src/app/partials/global/main-carousel-config/main-carousel-config.component.ts
@@ -4,35 +4,7 @@ import { NguCarouselConfig } from '@ngu/carousel';
 
 @Component({
   selector: 'app-main-carousel-config',
-  template: `
-    <ngu-carousel #myCarousel [inputs]="carouselTile" [dataSource]="carouselTileItems">
-      <ngu-tile *nguCarouselDef="let item; let i = index">
-
-        <ngu-carousel #myCarousel [inputs]="carouselTile" (carouselLoad)="carouselTileLoad(i)" [dataSource]="carouselTiles[i]">
-          <ngu-tile *nguCarouselDef="let item; let j = index">
-            <div class="tile" [style.background]="'url(' + item + ')'" style="min-height: 200px">
-              <h1>{{j}}</h1>
-            </div>
-          </ngu-tile>
-          <button NguCarouselPrev class="leftRs" [style.opacity]="myCarousel.isFirst ? 0.5:1">&lt;</button>
-          <button NguCarouselNext class="rightRs" [style.opacity]="myCarousel.isLast ? 0.5:1">&gt;</button>
-          <ul class="myPoint" NguCarouselPoint>
-            <li *ngFor="let j of myCarousel.pointNumbers; let j = index" [class.active]="j==myCarousel.activePoint" (click)="myCarousel.moveTo(j)"
-              [style.background]="'url(' + carouselTileItems[j] + ')'"></li>
-          </ul>
-        </ngu-carousel>
-      </ngu-tile>
-      <button NguCarouselPrev class="leftRs" [style.opacity]="myCarousel.isFirst ? 0.5:1">&lt;</button>
-      <button NguCarouselNext class="rightRs" [style.opacity]="myCarousel.isLast ? 0.5:1">&gt;</button>
-      <ul class="myPoint" NguCarouselPoint>
-        <li *ngFor="let i of myCarousel.pointNumbers; let i = index" [class.active]="i==myCarousel.activePoint"
-            (click)="myCarousel.moveTo(i)"
-          [style.background]="'url(' + carouselTileItems[i] + ')'"></li>
-      </ul>
-    </ngu-carousel>
-
-  `,
-  // templateUrl: './main-carousel-config.component.html',
+  templateUrl: './main-carousel-config.component.html',
   styleUrls: ['./main-carousel-config.component.scss']
 })
 export class MainCarouselConfigComponent implements OnInit {
@@ -67,14 +39,13 @@ export class MainCarouselConfigComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    console.log(j);
     this.carouselTileItems.forEach(el => {
       this.carouselTileLoad(el);
     });
   }
 
   public carouselTileLoad(j) {
-    console.log(j);
+    console.log('J: ' + j);
     const len = this.carouselTiles[j].length;
     if (len <= 30) {
       for (let i = len; i < len + 15; i++) {
